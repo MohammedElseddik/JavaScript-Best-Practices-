0e8198341098f6edc6d741927224381857ac55fe
Mohammed Elseddik Rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-04-04 20:50:32 +0200

    Fix linter issues

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: "/to-do-list-project/",
+    publicPath: '/to-do-list-project/',
   },
 
   module: {
